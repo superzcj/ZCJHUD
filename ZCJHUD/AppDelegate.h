49839ff40e3be2ec77c1b6c1622150fc679d4039
zcj <405911774@qq.com>
2016-09-19 19:44:28 +0800

    Initial Commit

---
+++ ZCJHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCJHUD
+//
+//  Created by zhangchaojie on 16/9/19.
+//  Copyright © 2016年 zhangchaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
